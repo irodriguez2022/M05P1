7f5be9143adb1ca5e7a54930ab7a11db13447e52
Isabel Rodriguez <irodriguez2022@ilg.cat>
2022-10-17 17:31:16 +0200

    Locale2

--- Program.java
+++ Program.java
@@ -2,8 +2,8 @@
  * Program.java        20.10.2020
  *
  * Program to calculate the area and the perimeter
- * of a circle knowing the radius in meter.
- * Programa . no , dfaf
+ * of a circle knowing the radicccccus in meter.
+ * Programa . no , dfafffffffff
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See http://www.gnu.org/licenses/gpl.html for more information.
